0f6644835ab60ca71a9f9599caf7728579d50170
Ali Abdullah Sadiq <123316557+AliAbdullahSadiq@users.noreply.github.com>
2024-05-22 18:44:39 +0500

    fixed OOP algorithms by making attributes private

--- Python/CAIE Algorithms/Communist OOP.py
+++ Python/CAIE Algorithms/Communist OOP.py
@@ -1,35 +1,36 @@
 class Proletariat:
     def __init__(self, name):
-        self.name = name
+        self.__name = name
 
     def work(self):
-        print(f"{self.name} is working for the collective!")
+        print(f"{self.__name} is working for the collective!")
 
 class PartyMember(Proletariat):
     def attend_meeting(self):
-        print(f"{self.name} attends a party meeting to discuss the revolution.")
+        print(f"{self._Proletariat__name} attends a party meeting to discuss the revolution.")
 
 class TechnologicalRevolution:
     def __init__(self, name):
-        self.name = name
+        self.__name = name
 
     def innovate(self):
-        print(f"{self.name} leads the technological revolution for the people!")
+        print(f"{self.__name} leads the technological revolution for the people!")
 
 class NationalizeIndustries:
     def __init__(self, industry):
-        self.industry = industry
+        self.__industry = industry
 
     def Nationalize(self):
-        print(f"Comrade Mila nationalizes the {self.industry} industry for the Motherland!")
+        print(f"Comrade Mila nationalizes the {self.__industry} industry for the Motherland!")
 
 class RevolutionaryLeader(PartyMember, TechnologicalRevolution):
     def __init__(self, name, revolution_name):
-        super().__init__(name)
-        self.revolution_name = revolution_name
+        PartyMember.__init__(self, name)
+        TechnologicalRevolution.__init__(self, name)
+        self.__revolution_name = revolution_name
 
     def lead_revolution(self):
-        print(f"{self.name} is leading the {self.revolution_name} revolution!")
+        print(f"{self._Proletariat__name} is leading the {self.__revolution_name} revolution!")
 
 worker1 = Proletariat("Ivan")
 party_member1 = PartyMember("Natalia")
--- Python/CAIE Algorithms/OOP.py
+++ Python/CAIE Algorithms/OOP.py
@@ -1,18 +1,18 @@
 class ClassName:
-    def __init__(self , attribute1 , attribute2): # Constructor
-        self.attribute1 = attribute1
-        self.attribute2 = attribute2
+    def __init__(self, attribute1, attribute2):  # Constructor
+        self.__attribute1 = attribute1
+        self.__attribute2 = attribute2
 
-    def GetAttribute1(self): # Getter
-        return self.attribute1
+    def get_attribute1(self):  # Getter
+        return self.__attribute1
     
-    def GetAttribute2(self): # Getter
-        return self.attribute2
+    def get_attribute2(self):  # Getter
+        return self.__attribute2
     
-I_AM_OBJECT = ClassName("DataType" , "DataType") # Object
+I_AM_OBJECT = ClassName("DataType", "DataType")  # Object
 
-Give_Me_Attribute1 = I_AM_OBJECT.GetAttribute1
-Give_Me_Attribute2 = I_AM_OBJECT.GetAttribute2
+Give_Me_Attribute1 = I_AM_OBJECT.get_attribute1
+Give_Me_Attribute2 = I_AM_OBJECT.get_attribute2
 
-print(Give_Me_Attribute1)
-print(Give_Me_Attribute2)
+print(Give_Me_Attribute1())
+print(Give_Me_Attribute2())
